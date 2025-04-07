a183fdb3a7fc1af664d99f9157faca39eebbb2f7
preethamKarra <preethamkarra97@gmail.com>
2025-04-07 12:22:30 -0400

    Initial commit

---
+++ PreeQuiz4App/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PreeQuiz4App.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
